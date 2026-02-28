8595b8650caa479bb60b6aaf76bec4c125ae7446
sonawaneshubham178-beep <sonawaneshubham178@gmail.com>
2026-02-28 11:13:37 +0530

    Add files via upload
    addition code

---
+++ addition.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+	int number1, number2,multiply;
+	
+	printf("enter numbers:");
+	scanf("%d %d",&number1,&number2);
+	multiply=number1*number2;
+	
+	printf("%d*%d=%d",number1,number2,multiply);
+	return 0;
+}
