8491894475baf0318738044904fba37052bddecf
sonawaneshubham178-beep <sonawaneshubham178@gmail.com>
2026-03-28 10:28:34 +0530

    Add files via upload

---
+++ 1 d array.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int array[5] = {10,20,30,40,50};
+	int i;
+	printf("Eements of the array are:\n");
+	
+	for(i = 0; 1 < 5; i++) {
+		printf("array[%d] = %d\n", i, array [i]);
+	}
+	return 0;
+	
+}
