27e2dc3852b578abc179e976962d4d53cdb88f17
sonawaneshubham178-beep <sonawaneshubham178@gmail.com>
2026-04-11 10:39:10 +0530

    Add files via upload

---
+++ array new.cpp
@@ -0,0 +1,95 @@
+ #include<stdio.h>
+ int main()
+ {
+ 	int array[100],n,i,largest, smallest;
+ 	printf("enter the size of array:");
+    scanf("%d", &n);
+	printf("Enter %d element:\n",n);
+	for(i=0; i<n;i++)
+	{
+		scanf("%d",&array[i]);
+		}	
+		largest = smallest = array[0];
+		for(i = 1; i < n; i++)
+		{ 
+		if(array[i] > largest)
+		largest = array[i];
+		if(array[i] < smallest)
+		smallest = array[i];
+		
+		}
+ 	printf("largest element:: %d\n", largest);
+ 	printf("smallest element : %d\n", smallest);
+ 	return 0;
+ }
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 
